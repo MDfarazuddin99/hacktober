e21d7dde5160073246ad84bd84fca29d0c890c40
MDfarazuddin99 <52372209+MDfarazuddin99@users.noreply.github.com>
2020-10-02 01:00:08 -0700

    first pull request commit

--- pr.py
+++ pr.py
@@ -1,5 +1,9 @@
-print('hello world')
+print('First Hacktober 2019')
 print('first pull request')
 print('second pull request')
 print('Third pull request')
 print('fourth pull request')
+
+print('Second Hacktober 2020')
+print('first pull request')
+
